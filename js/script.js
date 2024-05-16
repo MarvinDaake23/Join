2385110ec8a6ce15ee9e64ae47a8938d9b852dee
Christoph Völker <christoph@herrvoelker.de>
2024-05-16 07:41:54 +0200

    font

---
+++ js/script.js
@@ -0,0 +1,38 @@
+contacts = [
+  {
+    firstName: "Anton",
+    surName: "Mayer",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+  {
+    firstName: "Anja",
+    surName: "Schulz",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+  {
+    firstName: "Benedikt",
+    surName: "Ziegler",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+  {
+    firstName: "David",
+    surName: "Eisenberg",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+  {
+    firstName: "Eva",
+    surName: "Fischer",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+  {
+    firstName: "Emanuel",
+    surName: "Mauer",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+];
