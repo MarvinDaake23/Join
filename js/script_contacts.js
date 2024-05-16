df6b909cfbbedb69f15a7a093fd3c85acac3e491
Christoph Völker <christoph@herrvoelker.de>
2024-05-16 15:49:08 +0200

    contactliste v1

--- js/script_contacts.js
+++ js/script_contacts.js
@@ -1,43 +1,43 @@
-contacts = [
+let contacts = [
   {
     firstName: "Anton",
-    surName: "Mayer",
+    lastName: "Mayer",
     email: "antom@gmail.com",
     phoneNumber: "235325325",
   },
   {
     firstName: "Anja",
-    surName: "Schulz",
-    email: "antom@gmail.com",
+    lastName: "Schulz",
+    email: "anjaschulz@gmail.com",
     phoneNumber: "235325325",
   },
   {
     firstName: "Benedikt",
-    surName: "Ziegler",
-    email: "antom@gmail.com",
+    lastName: "Ziegler",
+    email: "ziegler@gmx.com",
     phoneNumber: "235325325",
   },
   {
     firstName: "David",
-    surName: "Eisenberg",
-    email: "antom@gmail.com",
+    lastName: "Eisenberg",
+    email: "eisenberg@googlemail.com",
     phoneNumber: "235325325",
   },
   {
     firstName: "Eva",
-    surName: "Fischer",
-    email: "antom@gmail.com",
+    lastName: "Fischer",
+    email: "fischer_eva@gmail.com",
     phoneNumber: "235325325",
   },
   {
     firstName: "Emanuel",
-    surName: "Mauer",
-    email: "antom@gmail.com",
+    lastName: "Mauer",
+    email: "e.mauer@gmail.com",
     phoneNumber: "235325325",
   },
   {
     firstName: "Tatjana",
-    surName: "Wolf",
+    lastName: "Wolf",
     email: "wolf@gmail.com",
     phoneNumber: "+49 2 2 2222 222 2",
   },
@@ -45,3 +45,21 @@ contacts = [
 
 /* Background-Colors for profiles */
 colors = ["#FF7A00", "#FFC700", "#9327FF", "#6E52FF", "#FC71FF", "#1FD7C1"];
+
+/**
+ * function to render the contacts
+ */
+function renderContacts() {
+  let container = document.getElementById("contactContainer");
+  container.innerHTML = "";
+
+  for (let index = 0; index < contacts.length; index++) {
+    const element = contacts[index];
+    container.innerHTML += `<div class="contactEntry">${element.firstName} ${element.lastName}</div>
+    <div class="initials">${element.firstName[0]}${element.lastName[0]}</div>
+    <div>${element.email}</div>
+    `;
+  }
+}
+
+
