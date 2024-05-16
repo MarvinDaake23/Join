5101881d43d742f8486cdf436e3ea89601d9d181
Christoph Völker <christoph@herrvoelker.de>
2024-05-16 11:55:30 +0200

    yes

---
+++ js/script_contacts.js
@@ -0,0 +1,47 @@
+contacts = [
+  {
+    firstName: "Anton",
+    surName: "Mayer",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+  {
+    firstName: "Anja",
+    surName: "Schulz",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+  {
+    firstName: "Benedikt",
+    surName: "Ziegler",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+  {
+    firstName: "David",
+    surName: "Eisenberg",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+  {
+    firstName: "Eva",
+    surName: "Fischer",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+  {
+    firstName: "Emanuel",
+    surName: "Mauer",
+    email: "antom@gmail.com",
+    phoneNumber: "235325325",
+  },
+  {
+    firstName: "Tatjana",
+    surName: "Wolf",
+    email: "wolf@gmail.com",
+    phoneNumber: "+49 2 2 2222 222 2",
+  },
+];
+
+/* Background-Colors for profiles */
+colors = ["#FF7A00", "#FFC700", "#9327FF", "#6E52FF", "#FC71FF", "#1FD7C1"];
