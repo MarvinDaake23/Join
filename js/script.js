61f84d8ca56768b2c973fb08e4f0d416af0154ff
Christoph Völker <christoph@herrvoelker.de>
2024-05-21 10:55:19 +0200

    functions

--- js/script.js
+++ js/script.js
@@ -11,11 +11,24 @@ function init() {
   inputSelector();
 }
 
-// database functions wichtig!!
+// database functions
 async function loadData(path = "") {
   let response = await fetch(BASE_URL + path + ".json");
   return (responseToJson = await response.json());
 }
+
+async function putData(path = "", data = {}) {
+  let response = await fetch(BASE_URL + path + ".json", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
+}
+
 
 async function includeHTML() {
   let includeElements = document.querySelectorAll("[w3-include-html]");
