8b01e12271227a9232c6b6d6fbe19cb3e0193c4c
Christoph Völker <christoph@herrvoelker.de>
2024-05-26 19:19:36 +0200

    added board.html v1

--- js/Templates.js
+++ js/Templates.js
@@ -69,3 +69,143 @@ function renderSingleContactHTML(id) {
         </div>
 `;
 }
+
+function inputFocusInput() {
+  return /*html */ `
+      <div class="subtaskSettingContainer">
+          <div onclick="inputClear()" class="imgContainerSubtaskCancle"></div>
+          <div class="seperator"></div>
+          <div onclick="loadSubtaskList()" class="imgContainerSubtaskSubmit"></div>
+      </div>
+      `;
+}
+
+function renderContactWrapper(element) {
+  return /*html */ `
+            <li>
+                <label class="contactWrapperItem">
+                    <input type="checkbox">
+                    <div class="contactNameContainer">
+                        <span>${element.firstName} ${element.lastName}</span>
+                        <div class="initials initSmall" style="background-color:${element.profileColor}">
+                            ${element.firstName[0]}${element.lastName[0]}
+                        </div>
+                    </div>
+                </label>
+            </li>
+    `;
+}
+
+function subtaskListInput(subtask) {
+  return /* html*/ `
+          <li class="liCat">
+              <div id="subtask">
+                  <div>${subtask}</div>
+              <div>
+          </li>
+      `;
+}
+
+function renderBoardTask(element, i) {
+  return /* html*/ `
+        <div draggable="true" ondragstart="startDragging(${element["id"]})" class="boardCard" onclick="loadBoardBigContainer(${i})">
+            <div id="" class="boardType">${element["type"]}</div>
+            <div class="boardTitle">${element["title"]}</div>
+            <div class="boardDescription">${element["description"]}</div>
+            <div class="progress">
+                <div class="progressBackground">
+                    <div class="progressbar" role="progresbar"></div>
+                </div>
+                <div>Subtasks</div>
+            </div>
+            <div class="boardTaskFooter">
+                <div class="boardTaskContacts" id="boardTaskContacts"></div>
+                <div id="boardTaskPrio"></div>
+            </div>
+        </div>
+    `;
+}
+
+function renderBoardTaskContacts(element) {
+  return /*html */ `
+        <div class="initials initSmall " style="background-color:${element["profilColor"]}">
+            <span>${element["firstName"][0]}${element["lastName"][0]}</span>
+        </div>
+    `;
+}
+
+function renderBoardBigContainer(i) {
+  return /*html */ `
+            <div class="boardBigContainerHeader">
+                <div class="boardBigContainerType" id="boardBigContainerType">${boardTasks[i]["type"]}</div>
+                <div class="boardBigContainerClose">X</div>
+            </div>
+            <div class="boardBigContainerTitle" id="boardBigContainerTitle">${boardTasks[i]["title"]}</div>
+            <div class="boardBigContainerDescription" id="boardBigContainerDescription">${boardTasks[i]["description"]}</div>
+            <ul>
+                <li class="boardBigContainerdate">
+                    <span>Due date:</span>
+                    <div class="boardBigContainerDateInput" id="boardBigContainerDateInput">${boardTasks[i]["dueDate"]}</div>
+                </li>
+                <li class="boardBigContainerPrio">
+                    <span>Priority:</span>
+                    <div>
+                        <div class="boardBigContainerPrioInput" id="boardBigContainerPrioInput">${boardTasks[i]["priority"]}</div>
+                        <div class="boardBigContainerPrioInputImg"></div>
+                    </div>
+                </li>
+            </ul>
+            <div class="boardBigContainerAssignedTo">
+                <span>Assigned To</span>
+                <ul id="boardBigContainerAssignedToContactsInput">
+                </ul>
+            </div>
+            <div>
+                <span>Subtasks</span>
+                <ul class="boardBigContainerSubtasks" id="boardBigContainerSubtasks">
+                </ul>
+            </div>
+    `;
+}
+
+function renderBoardBigContainerContacts(element) {
+  return /*html */ `
+        <li>
+            <div style="background-color:${element["profilColor"]}" class="contactIcon initials initSmall">${element["firstName"][0]} ${element["lastName"][0]}</div>
+            <div class="contactName">${element["firstName"]} ${element["lastName"]}</div>
+        </li>
+    `;
+}
+
+function renderBoardBigContainerSubtasks(element) {
+  return /* html*/ `
+        <li>
+            <div class="checkboxImg"></div>
+            <div class="boardBigContainerSubtasksSingleInput">${element["subtaskText"]}</div>
+        </li>
+    `;
+}
+
+function renderBoardTaskPlaceholderTodo() {
+  return /*html */ `
+        <div class="emptyPlaceholder">No Task To Do</div>
+    `;
+}
+
+function renderBoardTaskPlaceholderProgress() {
+  return /*html */ `
+        <div class="emptyPlaceholder">No Task in progress</div>
+    `;
+}
+
+function renderBoardTaskPlaceholderFeedback() {
+  return /*html */ `
+        <div class="emptyPlaceholder">No Task for Feedback</div>
+    `;
+}
+
+function renderBoardTaskPlaceholderDone() {
+  return /*html */ `
+        <div class="emptyPlaceholder">No Task done</div>
+    `;
+}
