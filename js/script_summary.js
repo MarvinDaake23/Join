10e47b64f95f1703028f6f4ad6b7b1b4d4b24c27
Christoph Völker <christoph@herrvoelker.de>
2024-05-28 18:46:22 +0200

    individ greeting by time

--- js/script_summary.js
+++ js/script_summary.js
@@ -3,5 +3,29 @@ let userAsText = localStorage.getItem("user");
 let user = JSON.parse(userAsText);
 
 function onLoadSummary() {
+  document.getElementById("greeting").innerHTML = greetUser();
   document.getElementById("loggedInUserName").innerHTML = user.User;
+}
+
+function greetUser() {
+  // create a new Date object
+  let now = new Date();
+  // get the current hour (from 0 to 23)
+  let hour = now.getHours();
+  let greeting = [];
+  // depending on actual hour, generate a greeting
+  if (hour >= 0 && hour < 6) {
+    // 0 - 5
+    greeting = "Good night";
+  } else if (hour >= 6 && hour < 12) {
+    // 6 - 11
+    greeting = "Good morning";
+  } else if (hour >= 12 && hour < 18) {
+    // 12 - 17
+    greeting = "Good afternoon";
+  } else if (hour >= 18 && hour < 24) {
+    // 18 - 23
+    greeting = "Good evening";
+  }
+  return greeting;
 }
