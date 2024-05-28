eadf520f195a6ab4796cbe5ff0b0fca47f839ad2
Christoph Völker <christoph@herrvoelker.de>
2024-05-28 14:51:22 +0200

    summary loginname

--- js/script_summary.js
+++ js/script_summary.js
@@ -1,1 +1,7 @@
 // load user from local storage
+let userAsText = localStorage.getItem("user");
+let user = JSON.parse(userAsText);
+
+function onLoadSummary() {
+  document.getElementById("loggedInUserName").innerHTML = user.User;
+}
