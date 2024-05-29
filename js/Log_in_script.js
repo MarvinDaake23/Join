cc3fd1178782522ff167353e0b33a9779c96d596
Christoph Völker <christoph@herrvoelker.de>
2024-05-29 15:04:47 +0200

    login update

--- js/Log_in_script.js
+++ js/Log_in_script.js
@@ -1,7 +1,7 @@
 rememberBulian = true;
 
-const BASE_URL =
-  "https://remotestorage-a7059-default-rtdb.europe-west1.firebasedatabase.app/";
+//const BASE_URL =
+//  "https://remotestorage-a7059-default-rtdb.europe-west1.firebasedatabase.app/";
 
 let responsetoJsonUsers = [];
 
