1f2bed6d7def2cf9095b1a6fa7d25e106f4ba9de
Christoph Völker <christoph@herrvoelker.de>
2024-05-31 07:39:46 +0200

    more cleaning up

--- js/Templates.js
+++ js/Templates.js
@@ -1,5 +1,5 @@
 function inputFocusInput() {
-    return /*html */ `
+  return /*html */ `
       <div class="subtaskSettingContainer">
           <div onclick="inputClear()" class="imgContainerSubtaskCancle"></div>
           <div class="seperator"></div>
@@ -9,7 +9,7 @@ function inputFocusInput() {
 }
 
 function renderContactWrapper(element, i) {
-    return /*html */ `
+  return /*html */ `
             <li>
                 <label class="contactWrapperItem">
                     <input type="checkbox" onchange="selectContacts(${i})">
@@ -25,7 +25,7 @@ function renderContactWrapper(element, i) {
 }
 
 function subtaskListInput(subtask) {
-    return /* html*/ `
+  return /* html*/ `
           <li class="liCat" onclick="editSubtaskList()">
               <div id="subtask" class="subtask">
                   <div>${subtask}</div>
@@ -40,7 +40,7 @@ function subtaskListInput(subtask) {
 }
 
 function renderBoardTask(element, i) {
-    return /* html*/ `
+  return /* html*/ `
         <div draggable="true" ondragstart="startDragging(${element["id"]})" class="boardCard" onclick="loadBoardBigContainer(${i})">
             <div id="" class="boardType">${element["type"]}</div>
             <div class="boardTitle">${element["title"]}</div>
@@ -60,7 +60,7 @@ function renderBoardTask(element, i) {
 }
 
 function renderBoardTaskContacts(element) {
-    return /*html */ `
+  return /*html */ `
         <div class="initials initSmall " style="background-color:${element["profilColor"]}">
             <span>${element["firstName"][0]}${element["lastName"][0]}</span>
         </div>
@@ -68,7 +68,7 @@ function renderBoardTaskContacts(element) {
 }
 
 function renderBoardBigContainer(i) {
-    return /*html */ `
+  return /*html */ `
             <div class="boardBigContainerHeader">
                 <div class="boardBigContainerType" id="boardBigContainerType">${boardTasks[i]["type"]}</div>
                 <img src="./assets/img/close.png" onclick="removeboardBigContainer()"  class="boardBigContainerClose">
@@ -107,7 +107,7 @@ function renderBoardBigContainer(i) {
 }
 
 function renderBoardBigContainerContacts(element) {
-    return /*html */ `
+  return /*html */ `
         <div class="boardContacts">
             <div style="background-color:${element["profilColor"]}" class="contactIcon initials initSmall">${element["firstName"][0]} ${element["lastName"][0]}</div>
             <div class="contactName">${element["firstName"]} ${element["lastName"]}</div>
@@ -116,7 +116,7 @@ function renderBoardBigContainerContacts(element) {
 }
 
 function renderBoardBigContainerSubtasks(element, j) {
-    return /* html*/ `
+  return /* html*/ `
         <div class="SubtaskRow">
             <img id="checkBox${j}" class="checkBox" onclick="done(${j})" src="../assets/img/Property 1=Default.png" >
             <div class="boardBigContainerSubtasksSingleInput">${element["subtaskText"]}</div>
@@ -125,31 +125,137 @@ function renderBoardBigContainerSubtasks(element, j) {
 }
 
 function renderBoardTaskPlaceholderTodo() {
-    return /*html */ `
+  return /*html */ `
         <div class="emptyPlaceholder">No Task To Do</div>
     `;
 }
 
 function renderBoardTaskPlaceholderProgress() {
-    return /*html */ `
+  return /*html */ `
         <div class="emptyPlaceholder">No Task in progress</div>
     `;
 }
 
 function renderBoardTaskPlaceholderFeedback() {
-    return /*html */ `
+  return /*html */ `
         <div class="emptyPlaceholder">No Task for Feedback</div>
     `;
 }
 
 function renderBoardTaskPlaceholderDone() {
-    return /*html */ `
+  return /*html */ `
         <div class="emptyPlaceholder">No Task done</div>
     `;
 }
 
 function renderSelectedContacs(element, i) {
-    return /*html */ `
-        <div style="background-color:${element['profileColor']}" class="initials initSmall">${element["firstName"][0]}${element["lastName"][0]}</div>
+  return /*html */ `
+        <div style="background-color:${element["profileColor"]}" class="initials initSmall">${element["firstName"][0]}${element["lastName"][0]}</div>
     `;
+}
+
+// CONTACTS PAGE
+function renderSingleContactEntryHTML(element, index) {
+  return `
+            <div id="singleContactEntry${index}" class="hover contactEntry" onclick="setActive(${index});singleContactView(${index})">
+              <div class="innerContactEntry">
+                  <div class="initials initSmall" style="background-color:${element.profileColor}">
+                      ${element.firstName[0]}${element.lastName[0]}
+                  </div>
+                  <div class="nameAndAdress">
+                      <span>${element.firstName} ${element.lastName}</span>
+                      <a href="mailto:${element.email}" class="emailAdress">${element.email}</a>
+                  </div>
+              </div>
+          </div>
+          `;
+}
+
+function renderContactSeperatorHTML(element) {
+  return `
+                <div class="seperatorLetter">${element.firstName[0]}</div>
+                <!-- <hr> -->
+                `;
+}
+
+function renderSingleContactHTML(id) {
+  return `
+            <div class="singleContactContainer">
+  
+                <div class="singleContact">
+                    <span style="background-color:${contacts[id].profileColor}" id="contactInitials" class="initials">${contacts[id].firstName[0]}${contacts[id].lastName[0]}</span>
+                    <div>
+                        <span id="contactName">${contacts[id].firstName} ${contacts[id].lastName}</span>
+                        <div class="editdelete">
+                            <a onclick="showEditContact(${id})"><img src="./assets/img/edit.svg">Edit</a>
+                            <a onclick="deleteContact(${id})"><img src="./assets/img/delete1.png">Delete</a>
+                        </div>
+                    </div>
+                </div>                
+    
+                <div class="contactInfos">
+                    <h3>Contact information</h3>
+                    <div class="infoItem">
+                        <h4>Email</h4>
+                        <a id="contactEmail" class="emailAdress" href="mailto:${contacts[id].email}">${contacts[id].email}</a>
+                    </div>
+                    <div class="infoItem">
+                        <h4>Phone</h4>
+                        <a id="contactPhoneNumber" class="phoneNumber" href="tel:${contacts[id].phoneNumber}">${contacts[id].phoneNumber}</a>
+                    </div>
+                </div>
+    
+                <img onclick="showMore()" id="moreButton" src="./assets/img/more1.png"/>
+    
+                <div id="moreButtonPopup">
+                    <a onclick="showEditContact(${id})"><img src="./assets/img/edit1.png" />Edit</a>
+                    <a onclick="deleteContact(${id})"><img src="./assets/img/delete1.png" />Delete</a>
+                </div>
+    
+            </div>
+    `;
+}
+
+function renderSingleContactMobileHTML(id) {
+  return `
+              <div class="singleContactContainerMobile">
+      
+          
+                  <div class="contactsHeadlineBox">
+                      <div class="contactsHeadline">
+                          <h2>Contacts</h2>
+                          <a><img onclick="backToContactList()" src="./assets/img/arrow-left-line.png" /></a>
+                      </div>
+                      <h3>Better with a team</h3>
+                      <hr />
+                  </div>
+            
+                  <div class="singleContact">
+                      <span style="background-color:${contacts[id].profileColor}" id="contactInitials" class="initials">${contacts[id].firstName[0]}${contacts[id].lastName[0]}</span>
+                      <div>
+                          <span id="contactName">${contacts[id].firstName} ${contacts[id].lastName}</span>
+                      </div>
+                  </div>                
+      
+                  <div class="contactInfos">
+                      <h3>Contact information</h3>
+                      <div class="infoItem">
+                          <h4>Email</h4>
+                          <a id="contactEmail" class="emailAdress" href="mailto:${contacts[id].email}">${contacts[id].email}</a>
+                      </div>
+                      <div class="infoItem">
+                          <h4>Phone</h4>
+                          <a id="contactPhoneNumber" class="phoneNumber" href="tel:${contacts[id].phoneNumber}">${contacts[id].phoneNumber}</a>
+                      </div>
+                  </div>
+      
+                  <img onclick="showMore()" id="moreButton" src="./assets/img/more1.png"/>
+      
+                  <div id="moreButtonPopup">
+                      <a onclick="showEditContact(${id})"><img src="./assets/img/edit1.png" />Edit</a>
+                      <a onclick="deleteContact(${id})"><img src="./assets/img/delete1.png" />Delete</a>
+                  </div>
+      
+              </div>
+      `;
 }
