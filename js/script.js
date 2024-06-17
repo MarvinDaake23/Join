6d0941a9716f4942f90a774563996e90546a0dcc
Christoph Völker <christoph@herrvoelker.de>
2024-06-17 08:18:13 +0200

    legal stuff

--- js/script.js
+++ js/script.js
@@ -29,11 +29,19 @@ function getRandomBackgroundColor() {
   return new_profileColor;
 }
 
-
-
 async function onLoadExtraPages() {
   await includeHTML();
   updateHeaderInitials();
+}
+
+async function onLoadExtraPagesExternal() {
+  await includeHTML();
+  updateLinksExternalPage();
+}
+
+async function updateLinksExternalPage() {
+  document.getElementById("privacyLink").href = "privacyPolicyExternal.html";
+  document.getElementById("legalNoticeLink").href = "legalNoticeExternal.html";
 }
 
 // database functions
