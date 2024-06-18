c575fd6aa2811db9bbdefd3c722b2360ce22a312
Christoph Völker <christoph@herrvoelker.de>
2024-06-18 15:41:21 +0200

    update

--- js/script.js
+++ js/script.js
@@ -172,7 +172,7 @@ function sleep(ms) {
  */
 function logOutUser() {
   localStorage.removeItem("user");
-  localStorage.getItem("cred");
+  localStorage.removeItem("cred");
   window.location = "index.html";
 }
 
